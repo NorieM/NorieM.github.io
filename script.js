59e13098f48805c1f06c4408e814b4be59e02f36
Norie MacEwan <50264139+NorieM@users.noreply.github.com>
2019-09-17 10:09:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function loadDoc() {
       return this.responseText;
     }
   };
-  xhttp.open("GET", "/data.csv", true);
+  xhttp.open("GET", "https://noriem.github.io/data.csv", true);
   xhttp.send();
 }
 
